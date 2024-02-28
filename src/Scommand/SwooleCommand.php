d4d14744491b6b9710cd6562503acfe77d79f951
chuanma <machuan5318@163.com>
2024-02-28 11:05:19 +0800

    scommand 执行完毕后停止进程

--- src/Scommand/SwooleCommand.php
+++ src/Scommand/SwooleCommand.php
@@ -2,6 +2,7 @@
 
 namespace Makuen\ThinkUtil\Scommand;
 
+use Swoole\Process\Pool;
 use think\console\Command;
 use think\console\Input;
 use think\console\input\Option;
@@ -12,7 +13,7 @@ use think\console\Output;
  * think-swoole 在命令行模式没有启动swoole服务 所以没有初始化连接池
  * 导致在command模式下无法并发操作数据库
  * 变更command继承此基类 会在调用命令时启动连接池 可以并发操作数据库
- * 但是也会对这个命令的进程进行守护 错误后会重启 完成后也不会退出 需注意
+ * 但是也会对这个命令的进程进行守护 错误后会重启 完成后会自动退出
  */
 abstract class SwooleCommand extends Command
 {
@@ -32,8 +33,11 @@ abstract class SwooleCommand extends Command
     protected function execute(Input $input, Output $output)
     {
         $manager = app(CommandManager::class);
-        $manager->addWorker(function ($pool, $workId) use ($input, $output) {
+        $manager->addWorker(function (Pool $pool, $workId) use ($input, $output) {
             $this->runInSwoole();
+
+            // 逻辑执行完毕后 停止程序
+            $pool->shutdown();
         }, $this->getName());
         $envName = $this->input->getOption('env');
         $manager->start($envName);
